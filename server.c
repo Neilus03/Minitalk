13c9c0ce78becd770194c85ed41842db756c9927
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-07-23 10:41:57 +0200

    this is gettingg betteeeer

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: nde-la-f <nde-la-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 11:51:19 by nde-la-f          #+#    #+#             */
-/*   Updated: 2023/07/22 16:49:26 by nde-la-f         ###   ########.fr       */
+/*   Updated: 2023/07/23 10:37:11 by nde-la-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,20 +45,18 @@ int	main(void)
 
 	ft_printf("Server PID: %d\n", getpid());
 	sa_usr1.sa_handler = &handle_sigusr1;
-	sigemptyset(&sa_usr1.sa_mask);
 	sa_usr1.sa_flags = 0;
 	sa_usr2.sa_handler = &handle_sigusr2;
-	sigemptyset(&sa_usr2.sa_mask);
 	sa_usr2.sa_flags = 0;
 	sigaction(SIGUSR1, &sa_usr1, NULL);
 	sigaction(SIGUSR2, &sa_usr2, NULL);
 	while (1)
-	{
 		pause();
-	}
 	return (0);
 }
 /*
+ALTERNATIVE BUT 2 GLOBAL VARS
+
 int		g_bit_position = 0;
 char	g_current_char = 0;
 
@@ -87,6 +85,32 @@ void	handle_sigusr2(int signum)
 	}
 	(void)signum;
 }
+
+int	main(void)
+{
+	struct sigaction	sa_usr1;
+	struct sigaction	sa_usr2;
+
+	ft_printf("Server PID: %d\n", getpid());
+	sa_usr1.sa_handler = &handle_sigusr1;
+	sigemptyset(&sa_usr1.sa_mask);
+	sa_usr1.sa_flags = 0;
+	sa_usr2.sa_handler = &handle_sigusr2;
+	sigemptyset(&sa_usr2.sa_mask);
+	sa_usr2.sa_flags = 0;
+	sigaction(SIGUSR1, &sa_usr1, NULL);
+	sigaction(SIGUSR2, &sa_usr2, NULL);
+	while (1)
+	{
+		pause();
+	}
+	return (0);
+}
+*/
+
+/*
+main with sigemptyset, it is recommended but not needed
+for this project so for clarity i removed it.
 
 int	main(void)
 {
