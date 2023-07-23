c60d9d008fe306aac7da8137d337cef56809f1b5
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-07-23 13:03:29 +0200

    Update server.c

--- server.c
+++ server.c
@@ -54,8 +54,35 @@ int	main(void)
 		pause();
 	return (0);
 }
+
 /*
-ALTERNATIVE BUT 2 GLOBAL VARS
+main with sigemptyset, it is recommended but not needed
+for this project so for clarity i removed it.
+
+int	main(void)
+{
+	struct sigaction	sa_usr1;
+	struct sigaction	sa_usr2;
+
+	ft_printf("Server PID: %d\n", getpid());
+	sa_usr1.sa_handler = &handle_sigusr1;
+	sigemptyset(&sa_usr1.sa_mask);
+	sa_usr1.sa_flags = 0;
+	sa_usr2.sa_handler = &handle_sigusr2;
+	sigemptyset(&sa_usr2.sa_mask);
+	sa_usr2.sa_flags = 0;
+	sigaction(SIGUSR1, &sa_usr1, NULL);
+	sigaction(SIGUSR2, &sa_usr2, NULL);
+	while (1)
+	{
+		pause();
+	}
+	return (0);
+}
+*/
+
+/*
+ALTERNATIVE: EASIER TO COMPREHEND BUT 2 GLOBAL VARS
 
 int		g_bit_position = 0;
 char	g_current_char = 0;
@@ -85,32 +112,6 @@ void	handle_sigusr2(int signum)
 	}
 	(void)signum;
 }
-
-int	main(void)
-{
-	struct sigaction	sa_usr1;
-	struct sigaction	sa_usr2;
-
-	ft_printf("Server PID: %d\n", getpid());
-	sa_usr1.sa_handler = &handle_sigusr1;
-	sigemptyset(&sa_usr1.sa_mask);
-	sa_usr1.sa_flags = 0;
-	sa_usr2.sa_handler = &handle_sigusr2;
-	sigemptyset(&sa_usr2.sa_mask);
-	sa_usr2.sa_flags = 0;
-	sigaction(SIGUSR1, &sa_usr1, NULL);
-	sigaction(SIGUSR2, &sa_usr2, NULL);
-	while (1)
-	{
-		pause();
-	}
-	return (0);
-}
-*/
-
-/*
-main with sigemptyset, it is recommended but not needed
-for this project so for clarity i removed it.
 
 int	main(void)
 {
